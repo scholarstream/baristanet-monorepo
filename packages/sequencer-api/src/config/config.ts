ad94e3922a6473306654423913dd48f23f436787
Dimas <dimasriat@gmail.com>
2025-03-19 02:11:53 +0700

    chore: update brewhouse address

--- packages/sequencer-api/src/config/config.ts
+++ packages/sequencer-api/src/config/config.ts
@@ -11,7 +11,7 @@ export const config: Config = {
   clearingChainData: {
     chainId: 421614, // arbitrum sepolia
     rpc: 'https://arbitrum-sepolia-rpc.publicnode.com',
-    brewHouseAddress: '0x0495f98b412c34526c5135eeff763d56cb31139a',
+    brewHouseAddress: '0xe151fe7360b77973133e2d3d1a0b47a386ba43cf',
   },
   brokerChainData: [
     {
