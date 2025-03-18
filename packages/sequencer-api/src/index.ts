6e238c0636fa29ae533c9f42c00a24382a56805c
Dimas <dimasriat@gmail.com>
2025-03-18 22:22:43 +0700

    feat: add express api

--- packages/sequencer-api/src/index.ts
+++ packages/sequencer-api/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to the RESTful API!');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
